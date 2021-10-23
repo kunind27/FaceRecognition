ee29d1711aead592ca4e886bb7d3efda80c19a04
Kunind Sahu <52349865+kunind27@users.noreply.github.com>
2021-10-23 19:51:10 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# For image pre-processing and data-loading
+from skimage import io
+import skimage.transform
+
+# Using PyTorch for Deep Learning Tasks
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms, utils
+
